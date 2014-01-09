9aa346861b9b87654a2f9eb5646d3032eb5b2650
Magnus Andersson <magnus.andersson@mollyware.se>
2014-01-09 23:48:05 +0100

    - Changed dependency versions

--- project/Build.scala
+++ project/Build.scala
@@ -4,24 +4,23 @@ import Keys._
 object Build extends sbt.Build{
 
   lazy val proj = Project(
-    "WebSockets",
+    "spray-websockets",
     file("."),
     settings =
       Defaults.defaultSettings ++ Seq(
-      organization  := "com.example",
+      organization  := "com.github.lihaoyi",
       version       := "0.1",
-      scalaVersion  := "2.10.2",
+      scalaVersion  := "2.10.3",
 
       resolvers ++= Seq(
         "typesafe repo"      at "http://repo.typesafe.com/typesafe/releases/",
         "spray"              at "http://repo.spray.io",
-        "spray nightly"      at "http://nightlies.spray.io/"
       ),
       libraryDependencies ++= Seq(
-        "io.spray"            %   "spray-can"     % "1.2-RC3",
+        "io.spray"            %   "spray-can"     % "1.2.0",
         "com.typesafe.akka"   %%  "akka-actor"    % "2.2.3",
         "com.typesafe.akka"   %%  "akka-testkit"  % "2.2.3" % "test",
-        "org.scalatest"       % "scalatest_2.10" % "2.0" % "test"
+        "org.scalatest"       % "scalatest_2.10"  % "2.0"   % "test"
       )
     )
   )
